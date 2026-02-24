e1b251fabbe798f5e850f5aaf12a7b88d460af12
Josh Yee <joshyee@gmail.com>
2026-02-24 15:22:05 -0800

    feat(weapons): add choice-generation logic to WeaponDatabase

--- game/weapons/scripts/weapon_database.gd
+++ game/weapons/scripts/weapon_database.gd
@@ -49,3 +49,35 @@ func get_upgradeable_weapons(weapon_manager: WeaponManager) -> Array[WeaponData]
 		if equipped != null and equipped.level < 5:
 			upgradeable.append(weapon)
 	return upgradeable
+
+
+# Generates up to 'count' level-up choices for the player
+# Returns Array of Dictionaries: { "type": "new"/"upgrade", "weapon_data": WeaponData }
+# UI needs to know if a choice adds a new weapon or upgrades existing
+func get_level_up_choices(weapon_manager: WeaponManager, count: int = 3) -> Array:
+	var choices: Array = []
+	
+	# Gather possible options
+	var unowned: Array[WeaponData] = get_unowned_weapons(weapon_manager)
+	var upgradeable: Array[WeaponData] = get_upgradeable_weapons(weapon_manager)
+	
+	# Can only offer new weapons if player has room
+	var can_add_new: bool = weapon_manager._get_weapon_count() < WeaponManager.MAX_WEAPONS
+	
+	# Build pool of all valid choices
+	var pool: Array = []
+	
+	if can_add_new:
+		for data in unowned:
+			pool.append({ "type": "new", "weapon_data": data })
+	
+	for data in upgradeable:
+		pool.append({ "type": "upgrade", "weapon_data": data })
+	
+	# Shuffle and pick up to 'count' choices
+    # TODO (Update 2): Add weighted selection when weapon pool grows beyond 6
+	pool.shuffle()
+	for i in range(mini(count, pool.size())):
+		choices.append(pool[i])
+	
+	return choices
